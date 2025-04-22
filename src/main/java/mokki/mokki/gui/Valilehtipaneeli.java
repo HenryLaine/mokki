b10477286461439a33ade3b8f4fe509d74e2d4f2
HenryLaine <laine.henry.m@gmail.com>
2025-04-22 05:09:20 +0300

    Muutettu käyttämään TabPane-elementtiä

--- src/main/java/mokki/mokki/gui/Valilehtipaneeli.java
+++ src/main/java/mokki/mokki/gui/Valilehtipaneeli.java
@@ -1,106 +1,29 @@
 package mokki.mokki.gui;
 
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 
 import java.util.ArrayList;
 
-public class Valilehtipaneeli extends VBox {
-    private FlowPane liukupaneeli = new FlowPane();
-    private ArrayList<StackPane> pinopaneelilista = new ArrayList<>();
-    private ArrayList<Rectangle> nelikulmiolista = new ArrayList<>();
-    private ArrayList<Text> tekstilista = new ArrayList<>();
-    private int aktiivinen = 0;
-    private String aktiivinenTyyli = "-fx-fill:#6EC5FF;-fx-stroke:black;-fx-stroke-width:1px;";
-    private String eiAktiivinenTyyli = "-fx-fill:#D5DEDC;-fx-stroke:black;-fx-stroke-width:1px;";
+public class Valilehtipaneeli extends TabPane {
+    private ArrayList<Tab> valilehtilista = new ArrayList<>();
 
-    public Valilehtipaneeli(String[] nimet, int leveys, int korkeus, int aktiivinen) {
-        for (String nimi : nimet) {
-            luoLehti(nimi, leveys, korkeus);
+    public Valilehtipaneeli(String[] valilehtienNimet) {
+        for (String nimi : valilehtienNimet) {
+            Tab lehti = new Tab(nimi);
+            valilehtilista.add(lehti);
+            this.getTabs().add(lehti);
         }
-        this.getChildren().add(liukupaneeli);
-        paivitaLehdet();
-        asetaTekstienTyyli("-fx-font-size:18px");
-        this.setStyle("-fx-border-color:black;-fx-border-width:1px;-fx-background-color:#C4CCC9;");
-        setAktiivinen(aktiivinen);
-        asetaToiminnallisuus();
+        this.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
     }
 
-    private void luoLehti(String nimi, int leveys, int korkeus) {
-        StackPane paneeli = new StackPane();
-        pinopaneelilista.add(paneeli);
-        Rectangle nelikulmio = new Rectangle(leveys, korkeus);
-        nelikulmiolista.add(nelikulmio);
-        Text teksti = new Text(nimi);
-        tekstilista.add(teksti);
-        paneeli.getChildren().addAll(nelikulmio, teksti);
-        liukupaneeli.getChildren().add(paneeli);
+    public ArrayList<Tab> getValilehtilista() {
+        return valilehtilista;
     }
 
-    public ArrayList<StackPane> getPinopaneelilista() {
-        return pinopaneelilista;
-    }
-
-    public ArrayList<Rectangle> getNelikulmiolista() {
-        return nelikulmiolista;
-    }
-
-    public ArrayList<Text> getTekstilista() {
-        return tekstilista;
-    }
-
-    public void setAktiivinenTyyli(String tyyli) {
-        aktiivinenTyyli = tyyli;
-        paivitaLehdet();
-    }
-
-    public void setEiAktiivinenTyyli(String tyyli) {
-        eiAktiivinenTyyli = tyyli;
-        paivitaLehdet();
-    }
-
-    private void paivitaLehdet() {
-        for (int i = 0; i < nelikulmiolista.size(); i++) {
-            Rectangle nelikulmio = nelikulmiolista.get(i);
-            if (i == aktiivinen) {
-                nelikulmio.setStyle(aktiivinenTyyli);
-            }
-            else {
-                nelikulmio.setStyle(eiAktiivinenTyyli);
-            }
-        }
-    }
-
-    public void asetaTekstienTyyli(String tyyli) {
-        for (Text teksti : tekstilista) {
-            teksti.setStyle(tyyli);
-        }
-    }
-
-    public void setAktiivinen(int indeksi) {
-        if (indeksi >= 0 && indeksi < pinopaneelilista.size()) {
-            aktiivinen = indeksi;
-            paivitaLehdet();
-        }
-    }
-
-    private void asetaToiminnallisuus() {
-        for (int i = 0; i < pinopaneelilista.size(); i++) {
-            StackPane paneeli = pinopaneelilista.get(i);
-            Rectangle nelikulmio = nelikulmiolista.get(i);
-                paneeli.setOnMouseEntered(e -> {
-                    nelikulmio.setStyle(aktiivinenTyyli);
-                });
-            int finalI = i;
-            paneeli.setOnMouseExited(e -> {
-                    if (finalI != aktiivinen) {
-                        nelikulmio.setStyle(eiAktiivinenTyyli);
-                    }
-                });
-        }
+    public void asetaFonttikoko(int fonttikoko) {
+        fonttikoko = (int)Math.round(fonttikoko * 1.3);
+        this.setStyle("-fx-font-size:" + fonttikoko + "px;");
     }
 
 }
