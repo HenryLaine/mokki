1f77e8978fadc06f44e0f8646d6a5962d252054f
HenryLaine <laine.henry.m@gmail.com>
2025-04-22 05:10:48 +0300

    Lisätty mahdollisuus hallita fonttikokoa

--- src/main/java/mokki/mokki/gui/Hallintapaneeli.java
+++ src/main/java/mokki/mokki/gui/Hallintapaneeli.java
@@ -10,18 +10,12 @@ public class Hallintapaneeli extends FlowPane {
     private Button rajaaPainike;
     private Button poistaRajauksetPainike;
     private Text rajauksetTeksti;
-    private String tyyli;
 
     public Hallintapaneeli(String[] painikkeidenNimet) {
-        tyyli = "-fx-font-size:18px;";
         lisaaPainike = new Button(painikkeidenNimet[0]);
         rajaaPainike = new Button(painikkeidenNimet[1]);
         poistaRajauksetPainike = new Button(painikkeidenNimet[2]);
         rajauksetTeksti = new Text("RAJAUKSET:\t\t\t");
-        lisaaPainike.setStyle(tyyli);
-        rajaaPainike.setStyle(tyyli);
-        poistaRajauksetPainike.setStyle(tyyli);
-        rajauksetTeksti.setStyle(tyyli);
         this.setHgap(20);
         this.setVgap(10);
         this.setPadding(new Insets(10, 10, 10, 10));
@@ -44,15 +38,12 @@ public class Hallintapaneeli extends FlowPane {
         return rajauksetTeksti;
     }
 
-    public String getTyyli() {
-        return tyyli;
-    }
-
-    public void setTyyli(String tyyli) {
-        this.tyyli = tyyli;
+    public void asetaFonttikoko(int fonttikoko) {
+        String tyyli = "-fx-font-size:" + fonttikoko + "px;";
         lisaaPainike.setStyle(tyyli);
         rajaaPainike.setStyle(tyyli);
         poistaRajauksetPainike.setStyle(tyyli);
         rajauksetTeksti.setStyle(tyyli);
     }
+
 }
