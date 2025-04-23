cad4f51d949b71083dbe7bc69f73bf7f937f3aee
HenryLaine <laine.henry.m@gmail.com>
2025-04-23 18:35:14 +0300

    Lisätty JavaDoc-kommentit

--- src/main/java/mokki/mokki/gui/Valilehtipaneeli.java
+++ src/main/java/mokki/mokki/gui/Valilehtipaneeli.java
@@ -2,27 +2,43 @@ package mokki.mokki.gui;
 
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-
 import java.util.ArrayList;
 
+/**
+ * Luokka toteuttaa käyttöliittymän välilehtipaneelin.
+ */
 public class Valilehtipaneeli extends TabPane {
+    /** Välilehdet sisältävä listarakenne */
     private ArrayList<Tab> valilehtilista = new ArrayList<>();
 
+    /**
+     * Luokan alustaja
+     * @param valilehtienNimet Välilehtien nimet
+     */
     public Valilehtipaneeli(String[] valilehtienNimet) {
+        // Luodaan välilehdet ja asetetaan ne paneeliin.
         for (String nimi : valilehtienNimet) {
             Tab lehti = new Tab(nimi);
             valilehtilista.add(lehti);
             this.getTabs().add(lehti);
         }
+        // Määritetään, että välilehtiä ei voi sulkea.
         this.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
     }
 
+    /**
+     * Metodi palauttaa välilehtilistan.
+     * @return välilehtilista
+     */
     public ArrayList<Tab> getValilehtilista() {
         return valilehtilista;
     }
 
+    /**
+     * Metodi muuttaa paneelin tekstin fonttikokoa.
+     * @param fonttikoko fontin koko
+     */
     public void asetaFonttikoko(int fonttikoko) {
-        fonttikoko = (int)Math.round(fonttikoko * 1.3);
         this.setStyle("-fx-font-size:" + fonttikoko + "px;");
     }
 
