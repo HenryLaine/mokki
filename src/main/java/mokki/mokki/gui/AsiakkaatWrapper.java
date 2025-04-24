3659fe7e0d2d44f739b091e0c0bc66eb010f4b8c
HenryLaine <laine.henry.m@gmail.com>
2025-04-24 05:47:18 +0300

    Lisätty wrapper luokka asiakkaille

---
+++ src/main/java/mokki/mokki/gui/AsiakkaatWrapper.java
@@ -0,0 +1,92 @@
+package mokki.mokki.gui;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * Wrapper-luokka asiakkaiden tiedoille. Luokka on tarkoitettu taulukkopaneeliin syötettävän tiedon tyypiksi.
+ */
+public class AsiakkaatWrapper {
+    private StringProperty nimi;
+    private StringProperty sahkoposti;
+    private StringProperty puhelinnumero;
+    private StringProperty tyyppi;
+    private StringProperty ytunnus;
+
+    /** Taulukkomääritykset, joita tarvitaan taulukon luomisessa */
+    private String[][] maaritykset;
+
+    /**
+     * Luokan alustaja
+     * @param nimi nimi
+     * @param sahkoposti sähköpostiosoite
+     * @param puhelinnumero puhelinnumero
+     * @param tyyppi tyyppi
+     * @param ytunnus Y-tunnus
+     */
+    public AsiakkaatWrapper(String nimi, String sahkoposti, String puhelinnumero,
+                            String tyyppi, String ytunnus) {
+
+        this.nimi = new SimpleStringProperty(nimi);
+        this.sahkoposti = new SimpleStringProperty(sahkoposti);
+        this.puhelinnumero = new SimpleStringProperty(puhelinnumero);
+        this.tyyppi = new SimpleStringProperty(tyyppi);
+        this.ytunnus = new SimpleStringProperty(ytunnus);
+
+        maaritykset = new String[][] {
+                {"Nimi", "String", "nimi"},
+                {"Sähköposti", "String", "sahkoposti"},
+                {"Puhelinnumero", "String", "puhelinnumero"},
+                {"Tyyppi", "String", "tyyppi"},
+                {"Y-tunnus", "String", "ytunnus"}
+        };
+    }
+
+    /**
+     * Metodi palauttaa nimi-kentän arvon.
+     * @return nimi
+     */
+    public String getNimi() {
+        return nimi.get();
+    }
+
+    /**
+     * Metodi palauttaa puhelinnumero-kentän arvon.
+     * @return puhelinnumero
+     */
+    public String getPuhelinnumero() {
+        return puhelinnumero.get();
+    }
+
+    /**
+     * Metodi palauttaa sähköposti-kentän arvon
+     * @return sähköposti
+     */
+    public String getSahkoposti() {
+        return sahkoposti.get();
+    }
+
+    /**
+     * Metodi palauttaa tyyppi-kentän arvon.
+     * @return tyyppi
+     */
+    public String getTyyppi() {
+        return tyyppi.get();
+    }
+
+    /**
+     * Metodi palauttaa ytunnus-kentän arvon.
+     * @return Y-tunnus
+     */
+    public String getYtunnus() {
+        return ytunnus.get();
+    }
+
+    /**
+     * Metodi palauttaa taulukkomääritykset.
+     * @return taulukkomääritykset
+     */
+    public String[][] getMaaritykset() {
+        return maaritykset;
+    }
+}
