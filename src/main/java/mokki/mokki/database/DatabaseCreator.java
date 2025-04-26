33a17ec58aec60b1161b134c0450af56075f799d
eeliloh <eeliloh@student.uef.fi>
2025-04-26 20:42:06 +0300

    Muokattu tietokannan luomista ja yhdistämistä

--- src/main/java/mokki/mokki/database/DatabaseCreator.java
+++ src/main/java/mokki/mokki/database/DatabaseCreator.java
@@ -1,22 +1,39 @@
 package mokki.mokki.database;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class DatabaseCreator {
 
-    private static final String SERVER_URL = "jdbc:mysql://localhost:3306/"; // ilman tietokantaa
-    private static final String DATABASE_NAME = "mokkikodit";
-    private static final String USER = "devuser";
-    private static final String PASSWORD = "Mokki666!";
+    private static String serverUrl;
+    private static String databaseName;
+    private static String user;
+    private static String password;
+
+    static {
+        try {
+            Properties props = new Properties();
+            props.load(new FileInputStream("database.properties"));
+            serverUrl = props.getProperty("DB_URL"); // Huom! Tarvitsee uuden avaimen!
+            databaseName = props.getProperty("DB_DATABASE");
+            user = props.getProperty("DB_USER");
+            password = props.getProperty("DB_PASSWORD");
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Tietokantakonfiguraation lataus epäonnistui.");
+        }
+    }
 
     public static void ensureDatabaseExists() {
-        try (Connection conn = DriverManager.getConnection(SERVER_URL, USER, PASSWORD);
+        try (Connection conn = DriverManager.getConnection(serverUrl, user, password);
              Statement stmt = conn.createStatement()) {
 
-            String sql = "CREATE DATABASE IF NOT EXISTS " + DATABASE_NAME;
+            String sql = "CREATE DATABASE IF NOT EXISTS " + databaseName;
             stmt.executeUpdate(sql);
             System.out.println("Tietokanta tarkistettu/luotu onnistuneesti.");
 
