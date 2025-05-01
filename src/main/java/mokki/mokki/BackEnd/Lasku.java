4f3ac2214ffcf94604b77ac986576dd40f8d498b
Antti Kukkonen <antti.kukkonen36@gmail.com>
2025-05-01 17:21:31 +0300

    Yhdistetty Mainin muutokset Dev Branchiin Aloitettu dokumentointia Lasku luokkaan

--- src/main/java/mokki/mokki/BackEnd/Lasku.java
+++ src/main/java/mokki/mokki/BackEnd/Lasku.java
@@ -1,13 +1,33 @@
 package mokki.mokki.BackEnd;
 
+/**
+ * Tämä luokka luo Lasku olion, jonka metodeja käytetääm DAO-luokassa yhdistämään tietokanta luokkiin
+ */
 public class Lasku {
+
+    /** Muuttuja kuvaa laskussa olevaa laskunumeroa*/
     private int laskunumero;
 
+    /** Muuttuja kuvaa laskulla olevan summan määrää*/
     private int lasku_summa;
+
+    /** Muuttuja kuvaa sen hetkistä laskun tilaa (maksettu/ei maksettu) */
     private boolean maksun_tila;
+
+    /** Muuttuja kuvaa laskun eräpäivää*/
     private int erapaiva;
+
+    /** Muuttuja kuvaa laskun päivämäärää*/
     private int lasku_paivamaara;
 
+    /**
+     * Konstruktori, jolla asetetaan arvot Lasku olion muuttujille
+     * @param laskunumero Laskulla oleva laskunumero
+     * @param laskuSumma Laskulla oleva summa
+     * @param maksunTila Laskun sen hetkinen tila (maksettu/ei maksettu)
+     * @param erapaiva Laskun eräpäivä
+     * @param laskuPaivamaara Laskun päivämäärä
+     */
     public Lasku(int laskunumero, int laskuSumma, boolean maksunTila, int erapaiva, int laskuPaivamaara) {
         this.laskunumero = laskunumero;
         lasku_summa = laskuSumma;
@@ -16,14 +36,26 @@ public class Lasku {
         lasku_paivamaara = laskuPaivamaara;
     }
 
+    /**
+     * Metodi asettaa parametrina tulleen numeron laskun numeroksi
+     * @param laskunumero Ilmaisee laskulla olevaa laskunumero
+     */
     public void setLaskunumero(int laskunumero) {
         this.laskunumero = laskunumero;
     }
 
+    /**
+     * Metodi palauttaa laskulla olevan laskunumeron
+     * @return Laskulla oleva laskunumero
+     */
     public int getLaskunumero() {
         return laskunumero;
     }
 
+    /**
+     * Metodi
+     * @param lasku_summa
+     */
     public void setLasku_summa(int lasku_summa) {
         this.lasku_summa = lasku_summa;
     }
