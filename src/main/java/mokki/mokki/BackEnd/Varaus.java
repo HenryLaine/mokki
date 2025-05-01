f0e868670b1a534aafa2ce18379c8296affa6123
Evergreated <tommy.liu@hotmail.fi>
2025-05-02 00:51:52 +0300

    Varaus luokka on kommentoitu javadoceilla

--- src/main/java/mokki/mokki/BackEnd/Varaus.java
+++ src/main/java/mokki/mokki/BackEnd/Varaus.java
@@ -2,6 +2,12 @@ package mokki.mokki.BackEnd;
 
 import java.util.Date;
 
+/**
+ * Luokka kuvaa varausta missä sisältyy varauksen tunnus, varaajien määrä,
+ * varauksien alku ja loppupäivämäärät
+ */
+
+
 public class Varaus
 {
 
@@ -10,6 +16,13 @@ public class Varaus
     private Date varausAlkuPvm;
     private Date varausLoppuPvm;
 
+    /**
+     * Alustaa uuden varauksen
+     * @param varausTunnus  varauksessa oleva varaustunnus
+     * @param varaajienMaara  varaajien määrä
+     * @param varausAlkuPvm    varauksen  alkupäivämäärä
+     * @param varausLoppuPvm   varauksen loppupäivämäärä
+     */
 
     Varaus(String varausTunnus, int varaajienMaara, Date varausAlkuPvm, Date varausLoppuPvm){
 
@@ -20,37 +33,80 @@ public class Varaus
     }
 
 
+    // setterit
+
+    /**
+     * metodi asettaa varaus tunnuksen
+      * @param varausTunnus  varauksen tunnus
+     */
+
 
     public void setVarausTunnus(String varausTunnus){
         this.varausTunnus = varausTunnus;
     }
 
+    /**
+     * metodi asettaa varaajien määrän
+     * @param varaajienMaara   varaajien määrä
+     */
+
     public void setVaraajienMaara(int varaajienMaara){
         this.varaajienMaara = varaajienMaara;
     }
 
+    /**
+     * metodi asettaa varauksen alkamispäivämäärän
+     * @param varausAlkuPvm   varauksen alkamispäivä
+     */
     public void setVarausAlkuPvm(Date varausAlkuPvm){
         this.varausAlkuPvm = varausAlkuPvm;
     }
 
+    /**
+     * metodi asettaa varauksen loppumispäivämäärän
+     * @param varausLoppuPvm   varauksen loppumispäivä
+     */
+
     public void setVarausLoppuPvm(Date varausLoppuPvm){
         this.varausLoppuPvm = varausLoppuPvm;
     }
+
+    // getterit
+
+    /**
+     * metodi palauttaa varaus tunnuksen
+     * @return   varaus tunnuksen
+     */
 
 
     public String getVarausTunnus(){
         return varausTunnus;
     }
 
+    /**
+     * metodi palauttaa varaajien määrän
+     * @return  varaajien määrän
+     */
+
     public int getVaraajienMaara(){
         return varaajienMaara;
     }
 
+    /**
+     * metodi palauttaa varauksen alkamispäivämäärän
+     * @return   varauksen alkamispäivämäärän
+     */
+
     public Date getVarausAlkuPvm(){
         return varausAlkuPvm;
     }
+
+    /**
+     * metodi palauttaa varauksen loppumispäivämäärän
+     * @return   varauksen loppumispäivämäärän
+     */
     public Date getVarausLoppuPvm(){
-        return getVarausLoppuPvm();
+        return varausLoppuPvm;
     }
 
 
