c05d80cc7e9f1b24a5fac9beb7fc88a5bd96a9f0
Evergreated <tommy.liu@hotmail.fi>
2025-05-09 15:22:42 +0300

    Tehty VarausDAO luokasta sopivaksi VarauksetWrapper luokan kanssa. (En ole varma)

--- src/main/java/mokki/mokki/dao/VarausDAO.java
+++ src/main/java/mokki/mokki/dao/VarausDAO.java
@@ -1,10 +1,12 @@
 package mokki.mokki.dao;
 import mokki.mokki.BackEnd.Varaus;
+import mokki.mokki.gui.testiluokatTaulukonDatalle.VarauksetWrapper;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 /** Luokka ottaa yhteyden tietokantaan ja muokkaa, lisää, poistaa ja listaa varauksia.
+*/
 
 public class VarausDAO {
     private Connection conn;
@@ -12,7 +14,23 @@ public class VarausDAO {
     public VarausDAO(Connection conn) {
         this.conn = conn;
     }
+
+    // tämä blokki tässä käyttää varauksetwrapper luokkaa ja tämä siis lisää varauksen
+    public void lisaaVaraus(VarauksetWrapper w) throws SQLException {
+        String sql = "INSERT INTO Varaus (aloitus_pvm, paattymis_pvm, henkilo_maara, sahkoposti,mokkiID) VALUES(?,?,?,?,?)";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setDate(1,java.sql.Date.valueOf(w.getAlkaa()));
+            stmt.setDate(2,java.sql.Date.valueOf(w.getPaattyy()));
+            stmt.setInt(3,1);
+            stmt.setString(4,w.getAsiakkaanSahkoposti());
+            stmt.setInt(5,Integer.parseInt(w.getKohteenTunnus()));
+            stmt.executeUpdate();
+        }
+    }
+
+
     // luo uusi varaus
+    /*
     public void luoVaraus(Varaus varaus) throws SQLException {
         String sql = "INSERT INTO Varaus (aloitus_pvm, paattymis_pvm, henkilo_maara, sahkoposti, mokkiID) VALUES (?, ?, ?, ?, ?)";
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
@@ -23,8 +41,30 @@ public class VarausDAO {
             stmt.setInt(5, varaus.getMokkiID());
             stmt.executeUpdate();
         }
-    }
+    }*/
+
+    // varauksen muokkaaminen joka käyttää varauksetwrapper luokkaa
+    public void muokkaaVarausta(VarauksetWrapper wrapper) throws SQLException {
+        String sql = "UPDATE Varaus SET aloitus_pvm = ?, paattymis_pvm = ?, sahkoposti = ?, mokkiID = ? WHERE varaustunnus = ?";
+
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setDate(1,java.sql.Date.valueOf(wrapper.getAlkaa()));
+            stmt.setDate(2,java.sql.Date.valueOf(wrapper.getPaattyy()));
+            stmt.setString(3,wrapper.getAsiakkaanSahkoposti());
+            stmt.setInt(4,Integer.parseInt(wrapper.getKohteenTunnus()));
+            stmt.setInt(5,Integer.parseInt(wrapper.getTunnus()));
+            stmt.executeUpdate();
+
+        }
+
+        }
+
+
+
+
+
     // varauksen muokkaaminen
+    /*
     public void muokkaaVarausta(Varaus varaus) throws SQLException {
         Varaus vanhaVaraus = haeVaraus(varaus.getVarausTunnus());
         if (vanhaVaraus == null) {
@@ -42,6 +82,8 @@ public class VarausDAO {
             stmt.executeUpdate();
         }
     }
+     */
+
     // apumetodi hakee yhden varauksen
     public Varaus haeVaraus(int varausID) throws SQLException {
         String sql = "SELECT * FROM Varaus WHERE varausID = ?";
@@ -72,7 +114,39 @@ public class VarausDAO {
         }
     }
 
+    // kaikkien varausten hakeminen käyttäen varauksetwrapper
+
+    public List<VarauksetWrapper> haeKaikkiWrapperVaraukset()throws SQLException {
+        List<VarauksetWrapper> lista = new ArrayList<>();
+        String sql = "SELECT v.*, a.nimi, a.sahkoposti FROM Varaus v JOIN AsiakasTiedotView a ON v.sahkoposti = a.sahkoposti";
+
+        try (Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                VarauksetWrapper varaus = new VarauksetWrapper(
+                        String.valueOf(rs.getInt("varaustunnus")),
+                        String.valueOf(rs.getInt("mokkiID")),
+                        rs.getString("nimi"),
+                        rs.getString("sahkoposti"),
+                        rs.getString("aloitus_pvm"),
+                        rs.getString("paattymis_pvm"),
+                        "Aktiivinen",
+                        ""
+                );
+                lista.add(varaus);
+            }
+
+        }
+        return lista;
+    }
+
+
+
+
+
+
     // kaikkien varausten hakeminen
+    /*
     public List<Varaus> haeKaikkiVaraukset() throws SQLException {
         List<Varaus> varaukset = new ArrayList<>();
         String sql = "SELECT * FROM Varaus";
@@ -85,7 +159,8 @@ public class VarausDAO {
             }
         }
         return varaukset;
-    }
+    } */
+
     // varausten hakeminen aikavälillä
     public List<Varaus> haeVarauksetPaivamaaranPerusteella(Date alku, Date loppu) throws SQLException {
         List<Varaus> varaukset = new ArrayList<>();
@@ -115,4 +190,5 @@ public class VarausDAO {
 
         return new Varaus(id, alku, loppu, maara, sahkoposti, mokkiID);
     }
-}*/
+}
+
